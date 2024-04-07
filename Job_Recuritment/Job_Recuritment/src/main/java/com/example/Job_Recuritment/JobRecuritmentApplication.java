bac4362b213603f7cb71a8e80671b7bbea9dd3ce
Sohankalburgi <sohankalburgi2004@gmail.com>
2024-04-07 12:32:13 +0530

    first commit

---
+++ Job_Recuritment/Job_Recuritment/src/main/java/com/example/Job_Recuritment/JobRecuritmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Job_Recuritment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecuritmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRecuritmentApplication.class, args);
+	}
+
+}
---
+++ Job_Recuritment/Job_Recuritment/src/test/java/com/example/Job_Recuritment/JobRecuritmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Job_Recuritment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRecuritmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

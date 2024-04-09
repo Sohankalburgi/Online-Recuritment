dfb2dc848311e90d914e5902b93e9bcaa3c6e748
ShivaCharanReddyK <110608023+ShivaCharanReddyK@users.noreply.github.com>
2024-04-09 18:25:29 +0530

    Added Spring Boot Backend

---
+++ OnlineRecruitmentSystem/src/main/java/com/example/OnlineRecruitmentSystem/OnlineRecruitmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlineRecruitmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRecruitmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRecruitmentSystemApplication.class, args);
+	}
+
+}
